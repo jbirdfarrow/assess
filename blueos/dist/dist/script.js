8894a903d5259ae2e16c2ebfb5355a2419955a16
bluecommunitysustainability <61095731+bluecommunitysustainability@users.noreply.github.com>
2021-08-28 15:20:21 -0400

    Add files via upload

---
+++ blueos/dist/dist/script.js
@@ -0,0 +1,13 @@
+//copy and pase in html and css //
+//in your texteditor//
+//change webpage webframe class height 550px;//
+//change background-sexe:500px; to 550px;//
+//you like please clik link like button//
+//clik link see about this on this page//
+//css add//
+//<link rel="stylesheett" type="text/css" href="anyname.css"/>//
+//same path on html//
+//otherwise added//
+//<head><style>----</style></head>//
+//save html file anyname.html//
+//save css file anyname.css//
